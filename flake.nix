69c0234a39b1435808e83ddd31bf71a1f551aefc
Vali0004 <diorcheats.vali@gmail.com>
2026-07-19 11:19:50 -0400

    Nix: extend systems

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
 
   outputs = { self, nixpkgs }:
   let
-    systems = [ "x86_64-linux" "x86_64-darwin" ];
+    systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
     forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
     packages = forAllSystems (system: let
